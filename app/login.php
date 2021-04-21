609bbb0a21caec35a2b0623e535805e0294b1de3
IakMastro <cse242017102@uniwa.gr>
2021-04-21 11:06:27 +0300

    Navbar

---
+++ app/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Κρασί</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="jumbotron text-center" style="margin-bottom: 0;">
+        <h1>Το κρασί του Μπάμπη Ποτερίδη</h1>
+        <p>Το νούμερο 1 μερός αγοράς κρασιού στο ίντερνετ!</p>
+    </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="index.php">Μενού</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <div class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="cart.php">Καλάθι</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="admin.php">Admin</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Σύνδεση/εγγραφή</a>
+                </li>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron text-center" style="margin-bottom: 0">
+        <p>Code with &#10084;&#65039; by iakmastro</p>
+    </div>
+</body>
+</html>
