94ab0ba3aac4b93d016e01cd161e90283836fa3e
IakMastro <cse242017102@uniwa.gr>
2021-05-10 21:27:49 +0300

    CSS

--- app/cart.php
+++ app/cart.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Καλάθι - Το κρασί του Μιχάλη Κερκόπουλου</title>
 
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -131,7 +132,7 @@
     </div>
     <br>
 
-    <div class="jumbotron text-center" style="margin-bottom: 0">
+    <div class="footer jumbotron text-center" style="margin-bottom: 0">
         <p>Coded with &#10084;&#65039; by iakmastro</p>
     </div>
 </body>
--- app/index.php
+++ app/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Το κρασί του Μιχάλη Κερκόπουλου</title>
 
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -94,7 +95,7 @@
         </div>
     </div>
 
-    <div class="jumbotron text-center" style="margin-bottom: 0">
+    <div class="footer jumbotron text-center" style="margin-bottom: 0">
         <p>Coded with &#10084;&#65039; by iakmastro</p>
     </div>
 </body>
